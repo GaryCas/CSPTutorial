c26f16237f52a4e0a84f2d9021a16d01e8c3f742
Garreth Cassar <rd019985@live.reading.ac.uk>
2016-11-05 18:45:31 +0000

    first altingchannel example

--- src/SendEvenIntsProcess.java
+++ src/SendEvenIntsProcess.java
@@ -1,42 +1,37 @@
-import org.jcsp.lang.CSProcess;
-import org.jcsp.lang.ChannelOutput;
+import org.jcsp.lang.*;
 import org.jcsp.util.Buffer;
 
 /**
  * Created by rd019985 on 05/11/2016.
  */
 public class SendEvenIntsProcess implements CSProcess {
-    private ChannelOutput outEven;
-    private ChannelOutput outOdd;
-    private ChannelOutput outOverFlow;
+    private AltingChannelOutput out0 = null;
+    private AltingChannelOutput out1 = null;
 
     private Buffer buffer = new Buffer(20);
 
-    public SendEvenIntsProcess(ChannelOutput outEven, ChannelOutput outOdd, ChannelOutput outOverFlow){
-        this.outEven = outEven;
-        this.outOdd = outOdd;
-        this.outOverFlow = outOverFlow;
+    public SendEvenIntsProcess(final AltingChannelOutput out0, final AltingChannelOutput out1){
+        this.out0 = out0;
+        this.out1 = out1;
     }
 
     @Override
     public void run() {
-        for(int i = 0; i <= 100; i++){
-            buffer.put(new Integer(i));
+        final Guard[] altChans = {out0, out1};
+        final Alternative alt = new Alternative(altChans);
 
-            if(buffer.getState() == 2) {
-                if(Integer.parseInt(buffer.startGet().toString()) % 2 == 0){
-                    System.out.println("Out channel" + outEven);
-                    outEven.write(buffer.get());
-                } else {
-                    System.out.println("Out channel" + outOdd);
-                    outOdd.write(buffer.get());
-                }
+        while(true){
+            switch (alt.select()){
+                case 0:
+                    out0.write(new Integer(0));
+                    System.out.println("out0 written");
+                    break;
+                case 1:
+                    out1.write(new Integer(1));
+                    System.out.println("out1 written");
+                    break;
             }
         }
-
-        while(buffer.startGet() != null){
-            System.out.println("Out channel" + outOverFlow);
-            outOverFlow.write(buffer.get());
-        }
     }
+
 }
